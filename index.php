3545438be7c849ad0bfbaa6094a17148bab8f907
Jesper Orb <jesper.orb@zocom.se>
2018-10-30 10:42:26 +0100

    Change title

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Login please</title>
+  <title>Login</title>
 </head>
 <body>
   <header>
